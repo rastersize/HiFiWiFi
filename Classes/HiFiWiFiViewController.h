34ab008bca9353d705b7ea31c0ece56f312c569f
Aron Cedercrantz <aron@cedercrantz.se>
2011-01-19 02:25:27 +0100

    Added two inline todo notes.

--- Classes/HiFiWiFiViewController.h
+++ Classes/HiFiWiFiViewController.h
@@ -10,6 +10,8 @@
 #import "MBProgressHUD.h"
 
 // TODO: Add multiple outcomes (perfect hit, good hit, lousy hit, miss) of high five
+// TODO: Add GameCenter rankings (most high fives, most high altitude high fives, most low altitude high fives, most high speed high fives, highest speed high five, highest altitude high five, lowest altitude high five).
+// TODO: Add GameCenter achivements (high altitude high five, low altitude high five, "fast five" (a high five done at greater than normal movement speed), "inaneley fast high five" (omg it's done so fast I can't believe the iPhone is still working), first high five, rookie high fiver (10), ..., ultimate high fiver (10000), moar).
 
 
 @interface HiFiWiFiViewController : UIViewController <MBProgressHUDDelegate, UIAccelerometerDelegate> {
